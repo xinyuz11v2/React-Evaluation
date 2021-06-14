4d8d098dc4e3999c18b0165cf02798b5190dbd4e
xinyuz11v2 <1371900121zou@gmail.com>
2021-06-14 13:18:37 -0700

    client env setup

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
 
-const App = () => <h1>You should see a red color text here</h1>;
+import '../client/index.css';
+
+const App = () => {
+    return(
+        <h1>You should see a red color text here</h1>
+    );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
